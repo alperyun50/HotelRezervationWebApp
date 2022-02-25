86a7b15f7532d311027abf4f7b06a89676f4384a
alper yavuz ün <yavuzun50@hotmail.com>
2022-02-25 19:52:05 +0300

    View component and destination DB added

---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+        IDestinationDAL _destinationDAL;
+
+        public DestinationManager(IDestinationDAL destinationDAL)
+        {
+            _destinationDAL = destinationDAL;
+        }
+
+        public void TAdd(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Destination TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Destination> TGetList()
+        {
+           return _destinationDAL.GetList();
+            
+        }
+
+        public void TUpdate(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HotelRezervationWebApp/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelRezervationWebApp.ViewComponents.Default
+{
+    public class _PopularDestinations : ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDAL());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+
+            return View(values);
+        }
+    }
+}
